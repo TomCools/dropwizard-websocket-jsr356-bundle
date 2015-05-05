154791adb9c1c65b9a22d6ae042e2da3c1d8d246
Tom Cools <tom.cools@live.be>
2015-05-05 07:25:41 +0200

    Added logic to WebsocketHandler.

--- src/main/java/be/tomcools/dropwizard/websocket/WebsocketHandler.java
+++ src/main/java/be/tomcools/dropwizard/websocket/WebsocketHandler.java
@@ -1,14 +1,25 @@
 package be.tomcools.dropwizard.websocket;
 
 import org.eclipse.jetty.websocket.jsr356.server.ServerContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.websocket.DeploymentException;
 
 public class WebsocketHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebsocketHandler.class);
     private final ServerContainer container;
 
     public WebsocketHandler(ServerContainer container) {
         this.container = container;
     }
 
-    public void addEndpoint(Class<?> aClass) {
+    public void addEndpoint(Class<?> endpointClass) {
+        //TODO implement checks on Class; (has annotation/interface... log parameters)
+        try {
+            container.addEndpoint(endpointClass);
+        } catch (DeploymentException e) {
+            LOGGER.error("Could not add websocket endpoint {} to the deployment.", endpointClass, e);
+        }
     }
 }
--- src/test/java/be/tomcools/dropwizard/websocket/WebsocketHandlerTest.java
+++ src/test/java/be/tomcools/dropwizard/websocket/WebsocketHandlerTest.java
@@ -1,4 +1,56 @@
 package be.tomcools.dropwizard.websocket;
 
+import org.eclipse.jetty.websocket.jsr356.server.ServerContainer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+
+import static org.mockito.Mockito.verify;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class WebsocketHandlerTest {
+
+    @Mock
+    private ServerContainer serverContainer;
+
+    @InjectMocks
+    private WebsocketHandler sut;
+
+    @Test
+    public void canConstructHandlerWithServerContainer() {
+        new WebsocketHandler(serverContainer);
+    }
+
+    @Test
+    public void whenAddEndpointIsCalledPassesObjectToServerContainer() throws DeploymentException {
+        sut.addEndpoint(TestEndpoint.class);
+
+        verify(serverContainer).addEndpoint(TestEndpoint.class);
+    }
+
+    @ServerEndpoint("/chat")
+    class TestEndpoint {
+        @OnOpen
+        public void open(Session session) {
+        }
+
+        @OnClose
+        public void close(Session session) {
+        }
+
+        @OnError
+        public void onError(Throwable error) {
+        }
+
+        @OnMessage
+        public void handleMessage(String message, Session session) {
+        }
+    }
+
 }
